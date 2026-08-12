cf4611e5952bfba5bb66bb35b1e7bf287c150400
Bruno Dechen <45558134+d3ch3n@users.noreply.github.com>
2026-08-12 15:30:32 -0300

    Bump plugin version to 1.1.1

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "DCH~Analog Way~Picturall Pro",
   Version = "0.0",
-  BuildVersion = "1.1.0.0",
+  BuildVersion = "1.1.1.0",
   Id = "ee9e56e9-f3fc-42d1-ba62-13911a5923f3",
   Author = "DCH",
   Description = "Analog Way Picturall Pro cue stack and playback control plugin"
